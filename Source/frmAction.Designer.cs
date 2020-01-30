4c0836300adb52c3f01cb16fe3b367e17fb86aca
Jouko Y <joukoy@gmail.com>
2020-01-30 13:52:36 +0200

    First upload

---
+++ Source/frmAction.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PCMBinBuilder
+{
+    partial class frmAction
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(235, 407);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(92, 31);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(10, 9);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(544, 380);
+            this.txtStatus.TabIndex = 1;
+            // 
+            // frmAction
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 450);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.btnClose);
+            this.Name = "frmAction";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmAction_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtStatus;
+    }
+}
---
+++ Source/frmAction.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PCMBinBuilder
+{
+    public partial class frmAction : Form
+    {
+        public frmAction()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAction_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void Logger(string LogText, Boolean newLine = true)
+        {
+            txtStatus.AppendText(LogText);
+            if (newLine)
+                txtStatus.AppendText(Environment.NewLine);
+        }
+        public void ExtractSegments()
+        {
+            string Fname = globals.SelectFile();
+            if (Fname.Length < 1)
+            {
+                Logger("No file selected");
+                return;
+
+            }
+
+            globals.GetPcmType(Fname);
+            if (globals.PcmType == "Unknown")
+            {
+                Logger("Unknown file");
+                return;
+            }
+
+            FrmAsk testDialog = new FrmAsk();
+            string Descr;
+
+            testDialog.TextBox1.Text = Path.GetFileName(Fname).Replace(".bin", "");
+            testDialog.AcceptButton = testDialog.btnOK;
+
+            // Show testDialog as a modal dialog and determine if DialogResult = OK.
+            if (testDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                // Read the contents of testDialog's TextBox.
+                Descr = testDialog.TextBox1.Text;
+            }
+            else
+            {
+                Descr = "";
+            }
+            testDialog.Dispose();
+            try
+            {
+
+                Logger("Reading segment addresses from file: " + Fname,false);
+                globals.GetSegmentAddresses(Fname);
+                Logger("(OK)");
+
+                byte[] buf = globals.ReadBinFile(Fname);
+                string OsFile = globals.PcmType + "-" + globals.GetOSid() + "-" + globals.GetOSVer();
+                OsFile = Path.Combine(Application.StartupPath, "OS", OsFile);
+
+                Logger("Writing OS segments",false);
+                globals.WriteSegmentToFile(OsFile + ".ossegment1", globals.PcmSegments[1].Start, globals.PcmSegments[1].End, buf);
+                globals.WriteSegmentToFile(OsFile + ".ossegment2", globals.PcmSegments[0].Start, globals.PcmSegments[0].End, buf);
+                Logger("(OK)");
+
+                //EEprom Data:
+                //globals.WriteSegmentToFile(OsFile + ".eepromdata", globals.PcmSegments[9].Start, globals.PcmSegments[9].End, buf);
+
+                StreamWriter sw = new StreamWriter(OsFile + ".txt");
+                sw.WriteLine(Descr);
+                sw.Close();
+
+                Logger("Writing calibration segments", false);
+
+                for (int s = 2; s <= 9; s++)
+                {
+                    string SegFname = Path.Combine(Application.StartupPath, "Calibration", globals.GetOSid() + "-" + globals.PcmSegments[s].Name + "-" + globals.PcmSegments[s].PN.ToString() + "-" + globals.PcmSegments[s].Ver) + ".calsegment";
+                    globals.WriteSegmentToFile(SegFname, globals.PcmSegments[s].Start, globals.PcmSegments[s].End, buf);
+                    sw = new StreamWriter(SegFname + ".txt");
+                    sw.WriteLine(Descr);
+                    sw.Close();
+                }
+                Logger("(OK)");
+
+            }
+            catch (Exception e) { 
+                Logger(e.Message);
+            }
+            finally
+            {
+                Logger("Extract done");
+
+            }
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+    }
+}
